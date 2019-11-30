6d8bb43c867e599582ff4d7041cfef2a7fab21e3
Avid3 <56166127+Avid3@users.noreply.github.com>
2019-11-29 22:12:28 -0500

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#reverse integer
+def reverse(val):
+    units=val%10
+    tens=val%100
+    tens=(tens-units)/10
+    hundreds=val%1000
+    hundreds=(hundreds-(tens*10+units))/100
+    reverse_=units*100+tens*10+hundreds
+    if reverse_==val:
+        return(1)
+    else:
+        return(0)
+    
+    
+    

a208554e2922d57ae462ee33934eb54e90ea5ba4
Avid3 <56166127+Avid3@users.noreply.github.com>
2020-04-21 23:41:51 -0400

    Add files via upload

---
+++ break.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 23:37:42 2020
+
+@author: Amey
+"""
+name='some'
+while True:
+    name=input()
+    if name=='your name':
+        break
+    else:
+        print(name)
+    
